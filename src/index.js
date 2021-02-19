6938135ce3e49c2ddb59e23d9cbe2be5c19b6f8c
Seroj <75806426+SerojDarkGray@users.noreply.github.com>
2021-02-19 20:01:23 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ function reducer(state = { count: 0 }, action) {
   switch(action.type){
     case 'INCREMENT':{
       return {
+        ...state,
         count: state.count + 1
       }
     }
     case 'DECREMENT':{
       return {
+        ...state,
         count: state.count - 1
       }
     }
